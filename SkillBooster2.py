6c50a546a2cc78501475fdc1b32ccd2ccb149edf
AngelaWuRX <146387665+AngelaWuRX@users.noreply.github.com>
2025-10-16 15:02:31 -0700

    Skill Booster 2

---
+++ SkillBooster2.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+
+image = cv2.imread('meme.png', cv2.IMREAD_GRAYSCALE)
+
+params = cv2.SimpleBlobDetector_Params()
+params.filterByArea = True
+params.minArea = 150
+params.filterByCircularity = False
+params.filterByConvexity = False
+params.filterByInertia = False
+
+detector = cv2.SimpleBlobDetector_create(params)
+keypoints = detector.detect(image)
+
+blank = np.zeros((1, 1))
+blobs = cv2.drawKeypoints(image, keypoints, blank, (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+cv2.imshow("Blobs", blobs)
+cv2.waitKey(3000)
+cv2.destroyAllWindows()
